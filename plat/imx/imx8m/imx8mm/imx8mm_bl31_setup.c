59d9be974f73ea0ef3ebfe30db97dc985e4ea28e
Andrey Zhizhikin <andrey.zhizhikin@leica-geosystems.com>
2022-07-08 14:34:57 +0000

    imx8mm: allow access to UARTs from A53 only
    TF-A sets the Peripheral Domain Access Permissions (RDC_PDAPn), where
    access to UART4 is limited to M4 and access to UART2 is limited to A53
    on i.MX8MM platform.
    
    Allow read and write access to all UARTs only from domain 0 (A53).
    
    Signed-off-by: Thomas Haemmerle <thomas.haemmerle@leica-geosystems.com>
    Signed-off-by: Andrey Zhizhikin <andrey.zhizhikin@leica-geosystems.com>

--- plat/imx/imx8m/imx8mm/imx8mm_bl31_setup.c
+++ plat/imx/imx8m/imx8mm/imx8mm_bl31_setup.c
@@ -64,8 +64,10 @@ static const struct imx_rdc_cfg rdc[] = {
 	RDC_MDAn(RDC_MDA_M4, DID1),
 
 	/* peripherals domain permission */
-	RDC_PDAPn(RDC_PDAP_UART4, D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_UART4, D0R | D0W),
+	RDC_PDAPn(RDC_PDAP_UART3, D0R | D0W),
 	RDC_PDAPn(RDC_PDAP_UART2, D0R | D0W),
+	RDC_PDAPn(RDC_PDAP_UART1, D0R | D0W),
 
 	/* memory region */
 
