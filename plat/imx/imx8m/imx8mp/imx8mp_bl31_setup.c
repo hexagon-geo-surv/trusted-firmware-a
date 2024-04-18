hexagon-geo-surv/trusted-firmware-a

commit 494cc47caf10c22ab486e297aeec6d9cf210398b

feat(imx8mp): restrict peripheral access to secure world
This restricts and locks all security relevant peripherals to only be
changeable by the secure world. Otherwise the normal world can simply
change the access settings and defeat all security measures put in
place.

Change-Id: I248ef8dd67f1de7e528c3da456311bb138b77540
Signed-off-by: Stefan Kerkmann <s.kerkmann@pengutronix.de>

plat/imx/imx8m/imx8mp/imx8mp_bl31_setup.c
@@ -64,8 +64,11 @@ static const struct imx_rdc_cfg rdc[] = {
 
 static const struct imx_csu_cfg csu_cfg[] = {
 	/* peripherals csl setting */
-	CSU_CSLx(CSU_CSL_OCRAM, CSU_SEC_LEVEL_2, UNLOCKED),
-	CSU_CSLx(CSU_CSL_OCRAM_S, CSU_SEC_LEVEL_2, UNLOCKED),
+	CSU_CSLx(CSU_CSL_OCRAM, CSU_SEC_LEVEL_2, LOCKED),
+	CSU_CSLx(CSU_CSL_OCRAM_S, CSU_SEC_LEVEL_2, LOCKED),
+	CSU_CSLx(CSU_CSL_RDC, CSU_SEC_LEVEL_3, LOCKED),
+	CSU_CSLx(CSU_CSL_TZASC, CSU_SEC_LEVEL_5, LOCKED),
+	CSU_CSLx(CSU_CSL_CSU, CSU_SEC_LEVEL_5, LOCKED),
 
 	/* master HP0~1 */
 

commit 40c7b64754c5105c22e41241c47d5150e5100027

fix(imx8mp): allow M7 access to peripherals post-security lock
After restricting and locking all security-relevant peripherals,
M7 can not access all the peripherals, the commit allow M7 access
to peripherals post-security lock.

Fixes:  494cc47ca - feat(imx8mp): restrict peripheral access to secure world

Signed-off-by: LI Qingwu <Qing-wu.Li@leica-geosystems.com.cn>

plat/imx/imx8m/imx8mp/imx8mp_bl31_setup.c
@@ -56,6 +56,23 @@ static const struct imx_rdc_cfg rdc[] = {
 	RDC_PDAPn(RDC_PDAP_UART2, D0R | D0W),
 	RDC_PDAPn(RDC_PDAP_UART1, D0R | D0W),
 
+	RDC_PDAPn(RDC_PDAP_RDC, D0R | D0W | D1R),
+
+	RDC_PDAPn(RDC_PDAP_eCSPI1, D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_UART4, D1R | D1W),
+
+	/*
+	 * Assignment the following to M7 only once the EVK is phased out.
+	 *
+	 * eCSPI2 on the EVK for the microchip,mpf-spi-fpga-mgr.
+	 * GPIO1 on EVK must be accessible from A52
+	 * UART3 on EVK is free, but maybe there are
+	 * some fumo setups may still use it,
+	 */
+	RDC_PDAPn(RDC_PDAP_eCSPI2, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_GPIO1, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_UART3, D0R | D0W | D1R | D1W),
+
 	/* memory region */
 
 	/* Sentinel */