hexagon-geo-surv/trusted-firmware-a

commit e85bde89289c059b8ffd6331f0536817e69eb945

feat(imx8mm): restrict peripheral access to secure world
This restricts and locks all security relevant peripherals to only be
changeable by the secure world. Otherwise the normal world can simply
change the access settings and defeat all security measures put in
place.

Change-Id: I484a2c8164e58b68256d829470e00d5ec473e266
Signed-off-by: Stefan Kerkmann <s.kerkmann@pengutronix.de>

plat/imx/imx8m/imx8mm/imx8mm_bl31_setup.c
@@ -79,7 +79,9 @@ static const struct imx_rdc_cfg rdc[] = {
 
 static const struct imx_csu_cfg csu_cfg[] = {
 	/* peripherals csl setting */
-	CSU_CSLx(0x1, CSU_SEC_LEVEL_0, UNLOCKED),
+	CSU_CSLx(CSU_CSL_RDC, CSU_SEC_LEVEL_3, LOCKED),
+	CSU_CSLx(CSU_CSL_TZASC, CSU_SEC_LEVEL_5, LOCKED),
+	CSU_CSLx(CSU_CSL_CSU, CSU_SEC_LEVEL_5, LOCKED),
 
 	/* master HP0~1 */
 

commit d00e535db76e2fcd4ffdc03764b03603a526e2e6

fix(imx8mm): unlock CSU_CSL_RDC temporary
temporary unlock CSU_CSL_RDC for imx8mm to make M4 working.

Fixes: e85bde8 feat(imx8mm): restrict peripheral access to secure world

Signed-off-by: LI Qingwu <Qing-wu.Li@leica-geosystems.com.cn>

plat/imx/imx8m/imx8mm/imx8mm_bl31_setup.c
@@ -79,7 +79,7 @@ static const struct imx_rdc_cfg rdc[] = {
 
 static const struct imx_csu_cfg csu_cfg[] = {
 	/* peripherals csl setting */
-	CSU_CSLx(CSU_CSL_RDC, CSU_SEC_LEVEL_3, LOCKED),
+	CSU_CSLx(CSU_CSL_RDC, CSU_SEC_LEVEL_0, UNLOCKED),
 	CSU_CSLx(CSU_CSL_TZASC, CSU_SEC_LEVEL_5, LOCKED),
 	CSU_CSLx(CSU_CSL_CSU, CSU_SEC_LEVEL_5, LOCKED),
 