hexagon-geo-surv/trusted-firmware-a

commit e85bde89289c059b8ffd6331f0536817e69eb945

feat(imx8mm): restrict peripheral access to secure world
This restricts and locks all security relevant peripherals to only be
changeable by the secure world. Otherwise the normal world can simply
change the access settings and defeat all security measures put in
place.

Change-Id: I484a2c8164e58b68256d829470e00d5ec473e266
Signed-off-by: Stefan Kerkmann <s.kerkmann@pengutronix.de>

plat/imx/imx8m/imx8mm/imx8mm_bl31_setup.c
@@ -79,7 +79,9 @@ static const struct imx_rdc_cfg rdc[] = {
 
 static const struct imx_csu_cfg csu_cfg[] = {
 	/* peripherals csl setting */
-	CSU_CSLx(0x1, CSU_SEC_LEVEL_0, UNLOCKED),
+	CSU_CSLx(CSU_CSL_RDC, CSU_SEC_LEVEL_3, LOCKED),
+	CSU_CSLx(CSU_CSL_TZASC, CSU_SEC_LEVEL_5, LOCKED),
+	CSU_CSLx(CSU_CSL_CSU, CSU_SEC_LEVEL_5, LOCKED),
 
 	/* master HP0~1 */
 

commit 9a84bafac22105d5442067fd226b981f6db6ef32

fix(imx8mm): unlock CSU_CSL_RDC temporary for crocodile
temporary unlock CSU_CSL_RDC for crocodile to make M4 working.

Fixes: e85bde892 feat(imx8mm): restrict peripheral access to secure world

Signed-off-by: LI Qingwu <Qing-wu.Li@leica-geosystems.com.cn>

plat/imx/imx8m/imx8mm/imx8mm_bl31_setup.c
@@ -50,6 +50,7 @@ static const mmap_region_t imx_mmap[] = {
 	MAP_REGION_FLAT(IMX_NS_OCRAM_BASE, IMX_NS_OCRAM_SIZE, MT_MEMORY | MT_RW), /* NS OCRAM */
 	MAP_REGION_FLAT(IMX_ROM_BASE, IMX_ROM_SIZE, MT_MEMORY | MT_RO), /* ROM code */
 	MAP_REGION_FLAT(IMX_DRAM_BASE, IMX_DRAM_SIZE, MT_MEMORY | MT_RW | MT_NS), /* DRAM */
+	//MAP_REGION_FLAT(IMX_TCM_BASE, IMX_TCM_SIZE, MT_MEMORY | MT_RW | MT_NS), /* TCM */
 	{0},
 };
 
@@ -65,13 +66,119 @@ static const struct imx_rdc_cfg rdc[] = {
 	/* Master domain assignment */
 	RDC_MDAn(RDC_MDA_M4, DID1),
 
+	# if 1
+	/* peripherals domain permission */
+	RDC_PDAPn(RDC_PDAP_GPIO1, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_GPIO2, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_GPIO3, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_ANA_TSENSOR, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_ANA_OSC, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_WDOG1, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_WDOG2, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_WDOG3, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_SDMA3, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_SDMA2, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_GPT1, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_GPT2, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_GPT3, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_ROMCP, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_OCOTP_CTRL, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_ANA_PLL, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_SNVS_HP, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_SRC, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_GPC, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_SEMAPHORE1, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_SEMAPHORE2, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_CSU, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_LCDIF, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_MIPI_DSI, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_CSI, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_MIPI_CSI, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_USB1, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_PWM1, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_PWM2, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_PWM3, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_PWM4, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_System_Counter_RD, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_System_Counter_CMP, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_System_Counter_CTRL, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_TZASC, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_USB2, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_PERFMON1, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_PERFMON2, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_PLATFORM_CTRL, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_QoSC, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_I2C1, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_I2C2, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_I2C3, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_SEMAPHORE_HS, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_SAI1, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_SAI2, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_SAI3, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_SAI5, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_SAI6, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_uSDHC1, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_uSDHC2, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_uSDHC3, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_PCIE_PHY1, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_SPBA2, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_QSPI, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_SDMA1, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_ENET1, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_SPDIF1, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_eCSPI1, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_eCSPI2, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_MICFIL, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_SPDIF2, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_SPBA1, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_CAAM, D0R | D0W | D1R | D1W),
+
+	RDC_PDAPn(RDC_PDAP_UART4, D0R | D0W),
+	RDC_PDAPn(RDC_PDAP_UART3, D0R | D0W),
+	RDC_PDAPn(RDC_PDAP_UART2, D0R | D0W),
+	RDC_PDAPn(RDC_PDAP_UART1, D0R | D0W),
+
+	RDC_PDAPn(RDC_PDAP_eCSPI3, D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_GPIO4,  D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_GPIO5,  D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_GPT4,   D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_GPT5,   D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_GPT6,   D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_I2C4,   D1R | D1W),
+
+	RDC_PDAPn(RDC_PDAP_CCM,	       D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_IOMUXC_GPR, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_IOMUXC,     D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_MU_A,       D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_MU_B,       D0R | D0W | D1R | D1W),
+	//RDC_PDAPn(RDC_PDAP_RDC,        D0R | D0W ), //XX
+
+	#else
 	/* peripherals domain permission */
 	RDC_PDAPn(RDC_PDAP_UART4, D0R | D0W),
 	RDC_PDAPn(RDC_PDAP_UART3, D0R | D0W),
 	RDC_PDAPn(RDC_PDAP_UART2, D0R | D0W),
 	RDC_PDAPn(RDC_PDAP_UART1, D0R | D0W),
 
+	RDC_PDAPn(RDC_PDAP_eCSPI3, D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_GPIO4,  D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_GPIO5,  D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_GPT4,   D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_GPT5,   D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_GPT6,   D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_I2C4,   D1R | D1W),
+
+	RDC_PDAPn(RDC_PDAP_CCM,	       D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_IOMUXC_GPR, D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_IOMUXC,     D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_MU_A,       D0R | D0W | D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_MU_B,       D0R | D0W | D1R | D1W),
+	//RDC_PDAPn(RDC_PDAP_RDC,        D0R | D0W | D1R ), //XX
+
+	#endif
 	/* memory region */
+	//RDC_MEM_REGIONn(1, 0x7feee000-IMX_DRAM_BASE >>1, 0x80000000-IMX_DRAM_BASE >>1, ENA|D0R|D0W|D1R|D1W),
+
 
 	/* Sentinel */
 	{0},

plat/imx/imx8m/imx8mm/include/imx_sec_def.h
@@ -42,6 +42,7 @@ enum rdc_mda_idx {
 
 /* RDC Peripherals index */
 enum rdc_pdap_idx {
+	RDC_PDAP_GPIO1 = 0,
 	RDC_PDAP_GPIO2 = 1,
 	RDC_PDAP_GPIO3 = 2,
 	RDC_PDAP_GPIO4 = 3,

plat/imx/imx8m/imx8mm/include/platform_def.h
@@ -122,6 +122,9 @@
 #define IMX_CAAM_RAM_SIZE		U(0x10000)
 #define IMX_DRAM_BASE			U(0x40000000)
 #define IMX_DRAM_SIZE			U(0xc0000000)
+#define IMX_TCM_BASE			U(0x7E0000)
+#define IMX_TCM_SIZE			U(0x40000)
+
 
 #define GPV_BASE			U(0x32000000)
 #define GPV_SIZE			U(0x800000)